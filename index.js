f7683e4c95c95bd6e43d7f9675ffed019cdb821b
Srimanth <srimanthraviprasad@gmail.com>
2021-06-14 08:50:12 +0000

    solution for super_over_league_program_using_react_js

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="main-bg">
+    <h1 className="main-head" text-align="center">
+      Super Over League
+    </h1>
+
+    <div className="device-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img-one"
+      />
+
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img-one"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
